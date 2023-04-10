d282f94056a5311ca8ea456588d3a2cbeffc266b
GabrielEstevesDev <112899019+GabrielEstevesDev@users.noreply.github.com>
2023-04-10 12:18:44 +0200

    Update DatabaseConnection.java

--- app/src/main/java/com/example/connectbd/bd/DatabaseConnection.java
+++ app/src/main/java/com/example/connectbd/bd/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String url = "jdbc:mysql://192.168.0.30:3306/androidbd";
+    private static final String url = "jdbc:mysql://10.10.29.198:3306/androidbd";
     private static final String user ="android";
     private static final String password ="";
     private static Connection connection;
